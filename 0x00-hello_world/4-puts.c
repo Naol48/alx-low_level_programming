7f0606c26397760a4bf4ba42fcf3768d51eec837
naol48 <abu487@gmail.com>
2022-09-08 11:05:33 -0700

    task 2 - 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,24 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *
+ * * main -Printing
+ *
+ * *
+ *
+ * *Return: Always 0 (successful)
+ *
+ * */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
+
+
