1e624d2e8256c8a3e4108c9cecbe2bd1ca473d99
naol48 <abu487@gmail.com>
2022-09-10 12:35:35 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,45 +1,23 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - print the string in the put function
  *
- *  *main-Prints the sizeof many var types
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     * /
- *
- *      int main(void)
- *
- *       {
- *
- *           char a;
- *
- *               int b;
- *
- *                   long int c;
- *
- *                       long long int d;
- *
- *                           float e;
- *
- *
- *
- *                               printf("Size of a char: %d byte(s)\n", sizeof(a));
- *
- *                                   printf("Size of an int: %d byte(s)\n", sizeof(b));
- *
- *                                       printf("Size of a long int: %d byte(s)\n", sizeof(c));
- *
- *                                           printf("Size of an long long int: %d byte(s)\n", sizeof(d));
- *
- *                                               printf("Size of a float: %d byte(s)\n", sizeof(e));
- *
- *                                                   return (0);
- *
- *                                                    } 
- *
- *
+ * Description: using the main function
+ * this program prints "programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("size of a char: %ld byte(s)\n", sizeof(c));
+	printf("size of an int: %1d byte(s)\n", sizeof(i));
+	printf("size of a long int: %ld byte(s)\n", sizeof(li));                        printf("size of a long long int: %1d byte(s)\n", sizeof(lli));
+        printf("size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
+}
