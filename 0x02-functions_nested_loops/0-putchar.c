d8a2ae311e67d9c2de5d79866e26b4801478fe35
naol48 <abu487@gmail.com>
2022-09-13 06:54:10 -0700

    tasko

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - prints _putchar , followed by a new line
- * Return: Always 0 (Success)
+ * main - prints _putchar
+ * Return: Always 0
  */
 int main(void)
 {
@@ -14,6 +14,7 @@ _putchar('h');
 _putchar('a');	
 _putchar('r');
 _putchar('\n'); 
-	    return(0);
+	
+  	return(0);
 
 }
