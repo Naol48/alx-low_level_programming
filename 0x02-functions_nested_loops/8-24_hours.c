625e3541565650d7c324d57e0b0ca91ea952f641
naol48 <abu487@gmail.com>
2022-09-14 12:12:46 -0700

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,16 +4,16 @@
  * jack_bauer - prints every minute of the day of jack bauer
  * starting from 00:00 to 23:59
  */
-void jack_baure(void)
+void jack_bauer(void)
 {
 	int i, j;
 
 	i = 0;
 
-	while (i < 24)
+	while (i < 23)
 	{
 		j = 0;
-		while (j < 60)
+		while (j < 59)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
