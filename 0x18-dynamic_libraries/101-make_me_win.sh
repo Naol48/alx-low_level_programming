877de873095d8c112a5bd5d91ab584c0d54cfd66
naol48 <abu487@gmail.com>
2022-12-16 10:50:57 -0800

    tasks

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#!/bin/bash
-wget -P /tmp https://raw.githubusercontent.com/naol48/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/library.so
-export LD_PRELOAD=/tmp/library.so
+wget -P .. https://raw.githubusercontent.com/naol48/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
